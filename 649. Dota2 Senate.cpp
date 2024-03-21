2683dc5dfc3ccf26ea640d71d4ce621e88061bd8
Moustafa Gamal <Moustafa.Gamal672@gmail.com>
2024-03-21 17:02:47 +0200

    Stack || Queue

---
+++ 649. Dota2 Senate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> qr, qd;
+        int x = senate.size();
+
+        for(int i = 0; i < x; i++)
+            (senate[i] == 'R')?qr.push(i):qd.push(i);
+        
+        while(!qr.empty() && !qd.empty()){
+            int r_index = qr.front(), d_index = qd.front();
+            qr.pop(), qd.pop();
+            (r_index < d_index)? qr.push(r_index + x) : qd.push(d_index + x);
+        }
+        
+        return (qr.size() > qd.size())? "Radiant" : "Dire";
+    }
+};
---
+++ 735. Asteroid Collision.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int x = asteroids.size();
+        stack<int>st;
+
+        for(int i = 0; i < x; i++){
+            int a = asteroids[i];
+            st.push(a);
+
+            while (st.size() > 1){
+                int top = st.top();
+                st.pop();
+                int prv = st.top();
+
+                if((prv > 0 && top > 0)||(top < 0 && prv < 0)){
+                    st.push(top);
+                    break;
+                }
+                if(prv > 0 && top < 0){
+                    int pos = abs(top);
+                    if(pos > prv) st.pop(), st.push(top);
+                    else if(pos == prv) st.pop();
+                }
+                else{
+                    st.push(top);
+                    break;
+                }
+            }
+
+        }
+
+        x = st.size();
+        vector<int>ans(x);
+        for(int i = x - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
