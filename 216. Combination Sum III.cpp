8728b8e2f5f674df0b1867cc2c640d6c863ebfc1
Moustafa Gamal <Moustafa.Gamal672@gmail.com>
2024-05-28 17:45:48 +0300

    Back on track? Maybe!

--- 216. Combination Sum III.cpp
+++ 216. Combination Sum III.cpp
@@ -1,12 +1,4 @@
 class Solution {
-public:
-    vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        backtrack(ans, temp, k, n, 1);
-        return ans;
-    }
-    
 private:
     void backtrack(vector<vector<int>>& ans, vector<int>& temp, int k, int n, int st) {
         if (n == 0 && k == 0) {
@@ -20,5 +12,12 @@ private:
             backtrack(ans, temp, k - 1, n - i, i + 1); 
             temp.pop_back();
         }
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        backtrack(ans, temp, k, n, 1);
+        return ans;
     }
 };
